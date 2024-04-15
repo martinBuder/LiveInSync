e94deec0c3da6728268195202f8e9ddc85589efd
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2024-04-15 17:54:51 +0200

    styled shooping chart

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,19 +10,41 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
-
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), TranslateModule.forRoot({
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    TranslateModule.forRoot({
       defaultLanguage: 'de',
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }).providers!, provideAnimationsAsync(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"mb-lifeinsync","appId":"1:484802075809:web:cba7a87de9b1fb8ef8e4e0","storageBucket":"mb-lifeinsync.appspot.com","apiKey":"AIzaSyAzXQ2zKXoHs6EHG98EcJ_pOX6cmGhMd_w","authDomain":"mb-lifeinsync.firebaseapp.com","messagingSenderId":"484802075809"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideDatabase(() => getDatabase())) ]
-}
+        deps: [HttpClient],
+      },
+    }).providers!,
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'mb-lifeinsync',
+          appId: '1:484802075809:web:cba7a87de9b1fb8ef8e4e0',
+          storageBucket: 'mb-lifeinsync.appspot.com',
+          apiKey: 'AIzaSyAzXQ2zKXoHs6EHG98EcJ_pOX6cmGhMd_w',
+          authDomain: 'mb-lifeinsync.firebaseapp.com',
+          messagingSenderId: '484802075809',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideDatabase(() => getDatabase())),
+    provideCharts(withDefaultRegisterables()),
+  ],
+};
--- src/app/components/feature/dashboard/cards/shopping-card/shopping-card.component.ts
+++ src/app/components/feature/dashboard/cards/shopping-card/shopping-card.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ChartData } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-shopping-card',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, BaseChartDirective],
   templateUrl: './shopping-card.component.html',
   styleUrl: './shopping-card.component.scss',
 })
-export class ShoppingCardComponent {}
+export class ShoppingCardComponent {
+  kategorieAmount: ChartData<'bar', { key: string; value: number }[]> = {
+    datasets: [
+      {
+        data: [
+          { key: 'Edeka', value: 20 },
+          { key: 'DM', value: 10 },
+          { key: 'Lidl', value: 8 },
+          { key: 'Tanke', value: 30 },
+        ],
+        parsing: {
+          xAxisKey: 'key',
+          yAxisKey: 'value',
+        },
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 205, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(201, 203, 207, 0.2)',
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)',
+          'rgb(201, 203, 207)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+}
