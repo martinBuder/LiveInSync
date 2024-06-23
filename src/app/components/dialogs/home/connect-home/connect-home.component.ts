837138f46927a37f8ce91c15ffceccc916ed91c4
Martin Buder <martin.buder@designyourstage.com>
2024-06-23 04:53:51 +0200

    in progress connect home

--- src/app/components/dialogs/home/connect-home/connect-home.component.ts
+++ src/app/components/dialogs/home/connect-home/connect-home.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+
+// LOAD_WASM().subscribe();
 
 @Component({
   selector: 'app-connect-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule, NgxScannerQrcodeModule],
   templateUrl: './connect-home.component.html',
-  styleUrl: './connect-home.component.scss'
+  styleUrl: './connect-home.component.scss',
 })
-export class ConnectHomeComponent {
-
-}
+export class ConnectHomeComponent {}
--- src/app/components/dialogs/home/share-home/share-home.component.ts
+++ src/app/components/dialogs/home/share-home/share-home.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { CurrentHomeService } from '../../../../services/frontend/current-home.service';
 import { QrCodeModule } from 'ng-qrcode';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeUtilService } from '../../../../services/utils/home-util.service';
 
 @Component({
   selector: 'app-share-home',
   standalone: true,
-  imports: [TranslateModule, QrCodeModule],
+  imports: [TranslateModule, QrCodeModule, MatIconModule],
   templateUrl: './share-home.component.html',
   styleUrl: './share-home.component.scss',
 })
 export class ShareHomeComponent {
-  constructor(protected currentHomeService: CurrentHomeService) {}
+  constructor(
+    protected currentHomeService: CurrentHomeService,
+    protected homeUtilService: HomeUtilService
+  ) {}
 }
